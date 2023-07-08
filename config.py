003778ce26ed2a37f93e4b552b11fa312bfa7097
sourabhlfjvj <108505537+sourabhlfjvj@users.noreply.github.com>
2023-07-08 18:31:52 +0530

    Update config.py
    Signed-off-by: sourabhlfjvj <108505537+sourabhlfjvj@users.noreply.github.com>

--- config.py
+++ config.py
@@ -9,19 +9,19 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "180"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/8cfaf7f865fda4c567f3c.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/8cfaf7f865fda4c567f3c.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/l_DIGITAL_WORLD_l")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/l_RAGHAV_l")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6028744448").split()))
 
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
